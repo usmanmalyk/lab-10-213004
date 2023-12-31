66c73696425cd5430ec9cfef91011adca7fe213e
usmanmalyk <97339674+usmanmalyk@users.noreply.github.com>
2023-12-31 20:35:34 +0500

    Add files via upload

---
+++ Lab10_213004-main/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+            var numQuery = from num in numbers where (num % 2) == 0 select num;
+
+            foreach (int num in numQuery)
+            {
+                Console.WriteLine("{0, 1}", num);
+            }
+
+            Console.ReadLine();
+        } 
+
+       
+    }
+}
